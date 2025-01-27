e59d59f3f90ae7a5bacdb67cb4e1e581d8af60ea
Kamila Semotánová <kamila.semotanova@gmail.com>
2025-01-27 15:37:14 +0100

    Update footer text

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,10 +1,9 @@
 import style from "./Footer.module.scss";
-import React from "react";
 
 const Footer = () => {
   return (
     <footer className={style.footer}>
-      <p className={style.copyright}>© 2023 For bars s.r.o.</p>
+      <p className={style.copyright}>© 2025 For bars events s.r.o.</p>
     </footer>
   );
 };
