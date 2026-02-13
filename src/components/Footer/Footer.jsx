424833f274371297f3473a3d8a24878dfa2b5bdf
Kamila Semotánová <kamila.semotanova@gmail.com>
2026-02-13 18:05:04 +0100

    Feat: add new section

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -3,6 +3,16 @@ import style from "./Footer.module.scss";
 const Footer = () => {
   return (
     <footer className={style.footer}>
+      <div className={style.info}>
+        <p className={style.copyright}>For bars events s.r.o.</p>
+        <p className={style.copyright}>IČO: 22190201</p>
+        <p className={style.copyright}>DIČ: CZ22190201</p>
+        <p className={style.copyright}>Vostrého 756/11, Braník, 14700, Praha</p>
+        <p className={style.copyright}>
+          Společnost vedená u Městského soudu v Praze pod spisovou značkou C
+          412316
+        </p>
+      </div>
       <p className={style.copyright}>© 2026 For bars events s.r.o.</p>
     </footer>
   );
